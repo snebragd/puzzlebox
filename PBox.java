6b21d9e1c11044745a06f538f3797bdcb0a6c9f0
Fredrik Stridsman <fredrik.stridsman@gmail.com>
2018-08-11 22:12:44 +0200

    The code. Now in git. Woho?

---
+++ PBox.java
@@ -0,0 +1,150 @@
+
+public class PBox {
+    
+    private static final int SIZE = 3;
+    private static final int PIECES = 6;
+    private static final int MAXSOL = 100;
+    
+    static int box[] = new int[SIZE*SIZE*SIZE];
+    static int savedBox[][] = new int[PIECES][SIZE*SIZE*SIZE];
+
+    static PPiece pieces[] = new PPiece[PIECES];
+
+    static int solutions[][] = new int[MAXSOL][SIZE*SIZE*SIZE];
+    static int nSolutions = 0;
+
+    /*
+     * Pieces enetered as array, row by row, starting from bottommost layer.
+     * Eg. The letter 'L' would look like: {1,1, 1,0, 1,0} (2 cols, 3 rows, 1 layer)
+     *     A 3D plus would be: {0,0,0, 0,1,0, 0,0,0,   0,1,0, 1,1,1, 0,1,0,   0,0,0, 0,1,0, 0,0,0} (3 cols,, 3 rows, 3 layers) 
+     */
+    static {
+	int p1[] = {1,1,1, 1,0,0};
+	pieces[0] = new PPiece(3, 2, 1, p1);
+
+	int p2[] = {1,1,1, 0,0,1,   0,1,0, 0,0,0};
+	pieces[1] = new PPiece(3, 2, 2, p2);
+	
+    	int p3[] = {1,1,1, 0,0,0,   1,0,0, 1,0,0};
+	pieces[2] = new PPiece(3, 2, 2, p3);
+	
+	int p4[] = {1,1, 0,0,   0,1, 0,1};
+	pieces[3] = new PPiece(2, 2, 2, p4);
+		
+	int p5[] = {1,1, 0,0,   1,0, 1,0};
+	pieces[4] = new PPiece(2, 2, 2, p5);
+	
+	int p6[] = {1,1,0, 0,1,0,   0,0,0, 0,1,1};
+	pieces[5] = new PPiece(3, 2, 2, p6);
+	
+    }
+
+    public static int get(int col, int row, int layer) {
+	return box[layer*SIZE*SIZE + row*SIZE + col];
+    }
+    
+    public static void set(int col, int row, int layer, int value) {
+	box[layer*SIZE*SIZE + row*SIZE + col] = value;
+    }
+
+
+    public static boolean placePiece(int n) {
+	PPiece p = pieces[n];
+
+	for(int t=0 ; (n>0 && t<24) || t<1 ; t++) { //skip rotation of first piece since that will only yield duplicate solutions
+		p.rotate(t);
+	
+		for(int lAdd=0 ; lAdd <= (SIZE-p.getLayers()) ; lAdd++) {	    
+		    for(int rAdd=0 ; rAdd <= (SIZE-p.getRows()) ; rAdd++) {
+			for(int cAdd=0 ; cAdd <= (SIZE-p.getCols()) ; cAdd++) {
+			    boolean fail=false;
+
+			    System.arraycopy(box, 0, savedBox[n], 0, SIZE*SIZE*SIZE);    
+			    //Try place
+			    for(int l=0; l<p.getLayers() && !fail; l++) {
+				for(int r=0; r<p.getRows() && !fail; r++) {
+				    for(int c=0; c<p.getCols() &&!fail; c++) {
+					int tmp;
+					try {
+					    tmp = p.get(c,r,l);
+					}
+					catch(ArrayIndexOutOfBoundsException e) {
+					    System.out.println("\nn:"+n+ " t:"+t+"c:"+c+" r:"+r+" l:"+l);
+					    System.out.println("dim:"+p.getCols()+","+p.getRows()+","+p.getLayers());
+					    throw e;
+					}
+					if(tmp > 0) {					    					    
+					    if(get(c+cAdd,r+rAdd,l+lAdd) > 0) {
+						fail=true;						
+					    }
+					    else {
+						set(c+cAdd,r+rAdd,l+lAdd, n+1);
+					    }
+					}
+				    }
+				}
+			    }
+			    if(!fail) {				
+				if(n == PIECES-1) {
+				    //complete solution!
+				    newSolution(); //post solution to be printed if unique, then search on for more solutions
+				}
+				else {
+				    //try next piece
+				    if(placePiece(n+1)) {
+					return true;
+				    }
+				}
+			    }
+
+			    //restore
+			    System.arraycopy(savedBox[n], 0, box, 0, SIZE*SIZE*SIZE);			    
+			}
+		    }	    
+		}
+	}
+	
+	return false;
+    }
+
+    private static void newSolution() {
+	boolean found = false;
+	for(int s=0 ; s<nSolutions ; s++) {
+	    if(java.util.Arrays.equals(box, solutions[s])) {
+		found = true;
+		break;
+	    }
+	}
+	if(!found) {
+	    //add new solution
+	    System.arraycopy(box, 0, solutions[nSolutions++], 0, SIZE*SIZE*SIZE);
+
+
+	    //print it
+	    System.out.println("Solution "+nSolutions);				    
+	    for(int i=0 ; i<SIZE ;i++) {
+		System.out.print("L" + i + "\t");	
+	    }
+	    System.out.println();			
+	    for(int row=SIZE-1 ; row >= 0 ; row--) {
+		for(int layer=0 ; layer < SIZE ; layer++) {
+		    
+		    for(int col=0 ; col < SIZE ; col++) {
+			System.out.print(get(col, row, layer) + " ");
+		    }
+		    System.out.print("\t");		
+		}
+		System.out.println();		
+	    }	    
+	}	    	
+    }
+    
+    public static void main(String args[]) {
+
+	placePiece(0);
+
+	if(nSolutions < 1) {
+	    System.out.println("Failed to lay puzzle! :-(");
+	}	
+    }
+}
---
+++ PPiece.java
@@ -0,0 +1,218 @@
+class PPiece {
+    private static final int COL=0;
+    private static final int ICOL=1;
+    private static final int ROW=2;
+    private static final int IROW=3;
+    private static final int LAY=4;
+    private static final int ILAY=5;
+
+    private int[] transform = new int[3];
+
+    private int rows, cols, layers;
+    private int[][][] shapeArr;
+       
+    public int getCols() { return axisToDim(transform[0]); }
+    public int getRows() { return axisToDim(transform[1]); }
+    public int getLayers() { return axisToDim(transform[2]); }
+    
+    private int axisToDim(int ax) {
+	switch(ax) {
+	case COL:
+	case ICOL:
+	    return cols;
+	case ROW:
+	case IROW:
+	    return rows;
+	default:
+	    return layers;
+	}
+    }    
+
+    public int get(int col, int row, int lay) {
+	int iCol=getCols()-col-1;
+	int iRow=getRows()-row-1;
+	int iLay=getLayers()-lay-1;
+
+	int tcol=-1, trow=-1, tlay=-1;
+
+	switch(transform[0]) {
+	case COL:           tcol=col; break;
+	case ICOL:          tcol=iCol; break;
+	case ROW:           trow=col; break;
+	case IROW:          trow=iCol; break;
+	case LAY:           tlay=col; break;
+	case ILAY: default: tlay=iCol; break;
+	}
+	switch(transform[1]) {
+	case COL:           tcol=row; break;
+	case ICOL:          tcol=iRow; break;
+	case ROW:           trow=row; break;
+	case IROW:          trow=iRow; break;
+	case LAY:           tlay=row; break;
+	case ILAY: default: tlay=iRow; break;
+	}
+	switch(transform[2]) {
+	case COL:           tcol=lay; break;
+	case ICOL:          tcol=iLay; break;
+	case ROW:           trow=lay; break;
+	case IROW:          trow=iLay; break;
+	case LAY:           tlay=lay; break;
+	case ILAY: default: tlay=iLay; break;
+	}
+	
+	return shapeArr[tcol][trow][tlay];
+    }
+    
+    void rotate(int twist) {		
+	switch(twist) {
+//up
+	case 0:
+	    transform[0] = COL;
+	    transform[1] = ROW;
+	    transform[2] = LAY;
+	    break;
+	case 1:
+	    transform[0] = IROW;
+	    transform[1] = COL;
+	    transform[2] = LAY;
+	    break;
+	case 2:
+	    transform[0] = ICOL;
+	    transform[1] = IROW;
+	    transform[2] = LAY;
+	    break;
+	case 3:
+	    transform[0] = ROW;
+	    transform[1] = ICOL;
+	    transform[2] = LAY;
+	    break;
+//down
+	case 4: //icol, row, ilay	    
+	    transform[0] = ICOL;
+	    transform[1] = ROW;
+	    transform[2] = ILAY;
+	    break;
+	case 5: //irow, icol, ilay
+	    transform[0] = IROW;
+	    transform[1] = ICOL;
+	    transform[2] = ILAY;
+	    break;
+	case 6: //col, irow, ilay
+	    transform[0] = COL;
+	    transform[1] = IROW;
+	    transform[2] = ILAY;
+	    break;
+	case 7: //row, col, ilay
+	    transform[0] = ROW;
+	    transform[1] = COL;
+	    transform[2] = ILAY;
+	    break;
+
+//left
+	case 8: //ilay, row, col
+	    transform[0] = ILAY;
+	    transform[1] = ROW;
+	    transform[2] = COL;
+	    break;
+	case 9: //irow, ilay, col
+	    transform[0] = ILAY;
+	    transform[1] = ICOL;
+	    transform[2] = ROW;
+	    break;
+	case 10: //lay, irow, col
+	    transform[0] = ILAY;
+	    transform[1] = IROW;
+	    transform[2] = ICOL;
+	    break;
+	case 11: //row, lay, col
+ 	    transform[0] = ILAY;
+	    transform[1] = COL;
+	    transform[2] = IROW;
+	    break;
+
+//right
+	case 12: 
+	    transform[0] = LAY;
+	    transform[1] = ROW;
+	    transform[2] = ICOL;
+	    break;
+	case 13: 
+	    transform[0] = LAY;
+	    transform[1] = ICOL;
+	    transform[2] = IROW;
+	    break;
+	case 14: 
+	    transform[0] = LAY;
+	    transform[1] = IROW;
+	    transform[2] = COL;
+	    break;
+	case 15: 
+ 	    transform[0] = LAY;
+	    transform[1] = COL;
+	    transform[2] = ROW;
+	    break;
+
+//away
+	case 16: 
+	    transform[0] = COL;
+	    transform[1] = LAY;
+	    transform[2] = IROW;
+	    break;
+	case 17: 
+	    transform[0] = ROW;
+	    transform[1] = LAY;
+	    transform[2] = COL;
+	    break;
+	case 18: 
+	    transform[0] = ICOL;
+	    transform[1] = LAY;
+	    transform[2] = ROW;
+	    break;
+	case 19: 
+ 	    transform[0] = IROW;
+	    transform[1] = LAY;
+	    transform[2] = ICOL;
+	    break;
+
+//toward
+	case 20: 
+	    transform[0] = COL;
+	    transform[1] = ILAY;
+	    transform[2] = ROW;
+	    break;
+	case 21: 
+	    transform[0] = IROW;
+	    transform[1] = ILAY;
+	    transform[2] = COL;
+	    break;
+	case 22: 
+	    transform[0] = ICOL;
+	    transform[1] = ILAY;
+	    transform[2] = IROW;
+	    break;
+	case 23: 
+ 	    transform[0] = ROW;
+	    transform[1] = ILAY;
+	    transform[2] = ICOL;
+	    break;
+
+	}		
+    }
+    
+    PPiece(int p_cols, int p_rows, int p_layers, int p_shape[]) {
+	cols = p_cols;
+	rows = p_rows;
+	layers = p_layers;	
+
+	shapeArr = new int[p_cols][p_rows][p_layers];
+	int i=0;
+	for(int l=0 ; l<layers ; l++) {
+	    for(int r=0 ; r<rows ; r++) {
+		for(int c=0 ; c<cols ; c++) {
+		    shapeArr[c][r][l] = p_shape[i++];
+		}
+	    }
+	}	    
+    }
+
+}
